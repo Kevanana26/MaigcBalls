2ff586de5462030c9e8a31a4c537ddfd1a47293f
Kevanana26 <525kpiccinich@frhsd.com>
2023-12-14 10:41:03 -0500

    added a bit more to the js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function shakeMagic8Ball(){
     if(question === null){
         document.getElementById('response-text').innerText = 'Please ask a question, please and thank you'
         document.getElementById('response-text').classList = 'display text-danger'
+        document.getElementById('response-image').src = '#BADIMAGE' //fix this shit later dog//
     }
-
+    if(!question.trim()){
+        alert('Buddy, please enter an actual question that can be answered')
+    }
 }
